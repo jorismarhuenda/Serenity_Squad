335268f90277bca9ba86ea5f44b840ccee79e9f0
Joris Marhuenda <marhuendajoris@gmail.com>
2024-06-02 13:40:16 +0200

    First commit

---
+++ Serenity Squad/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Serenity Squad
+//
+//  Created by marhuenda joris on 02/06/2024.
+//
+
+import SwiftUI
+
+struct Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color_Previews: PreviewProvider {
+    static var previews: some View {
+        Color()
+    }
+}
---
+++ Serenity Squad/QuestionnaireView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionnaireView.swift
+//  Serenity Squad
+//
+//  Created by marhuenda joris on 02/06/2024.
+//
+
+import SwiftUI
+
+struct QuestionnaireView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionnaireView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionnaireView()
+    }
+}

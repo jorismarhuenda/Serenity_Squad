962959ad2ec06ffe0076ae9b9474ad98bb474c83
Joris Marhuenda <marhuendajoris@gmail.com>
2024-06-02 13:22:06 +0200

    Initial Commit

---
+++ Serenity Squad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Serenity Squad
+//
+//  Created by marhuenda joris on 02/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Serenity Squad/Serenity_SquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Serenity_SquadApp.swift
+//  Serenity Squad
+//
+//  Created by marhuenda joris on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Serenity_SquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
